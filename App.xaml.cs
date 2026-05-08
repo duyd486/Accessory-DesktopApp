1081475a2a21c05b79a219b87759ac6fa6875332
duyd486 <90164590+duyd486@users.noreply.github.com>
2026-05-08 20:54:53 +0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Accessory_DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
